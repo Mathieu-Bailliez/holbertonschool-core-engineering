a7924d0dd9e5cdf289e98275abf1cba7ed4dce82
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-04 23:41:26 +0200

    fix: pycodestyle space and add a \ at the end of the line for respect the  Maximum char in one line

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
-#Define variable
-Langage=("Python")
-version=(3)
-pi=(3.141592653589793)
-booleen=(10>1)
+# Define variable
 
-#Output info
-output_string = f'Language: {Langage} \nVersion: {version} \nPi approx: {pi:.2f} \nComputation valid: {booleen}'
+Langage = ("Python")
+version = (3)
+pi = (3.141592653589793)
+booleen = (10 > 1)
+
+# Output info
+
+output_string = f'Language: {Langage} \nVersion: {version} \n\
+Pi approx: {pi:.2f} \nComputation valid: {booleen}'
+
 print(output_string)
