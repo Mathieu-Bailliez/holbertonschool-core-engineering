328f8fc66691c05ae8b96290b588b08d4c174026
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-04 23:47:22 +0200

    Fix: install the pycodestyle 2.7.0 and fix the whitespace after : in line 13

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -2,14 +2,14 @@
 
 # Define variable
 
-Langage = ("Python")
+Language = ("Python")
 version = (3)
 pi = (3.141592653589793)
 booleen = (10 > 1)
 
 # Output info
 
-output_string = f'Language: {Langage} \nVersion: {version} \n\
-Pi approx: {pi:.2f} \nComputation valid: {booleen}'
+output_string = f'Language: {Language} \nVersion: {version} \n\
+Pi approx: {pi: .2f} \nComputation valid: {booleen}'
 
 print(output_string)
