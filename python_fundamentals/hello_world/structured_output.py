91c447faf3d53ee051d292aee5534eac323db7a1
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-05 00:45:15 +0200

    feat:Totally replace the structure for respect the holberton rules

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 # Define variable
-
 language = ("Python")
 version = (3)
 pi = (3.141592653589793)
@@ -9,7 +8,13 @@ booleen = (10 > 1)
 
 # Output info
 
-output_string = f'Language: {language} \nVersion: {version} \n\
-Pi approx: {pi: .2f} \nComputation valid: {booleen}'
+# formatted string interpolation
+print(f"Language: {language}")
 
-print(output_string)
+print("Version:", version)
+
+# The float must be derived from a numeric value and formatted to two decimals
+print("Pi approx: %.2f" % pi)
+
+# The boolean must result from evaluating a comparison expression
+print("Computation valid:", booleen)
