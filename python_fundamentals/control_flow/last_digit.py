0667cfbbf9882828e9534bd57d52037a703318a9
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-06 17:54:04 +0200

    Fix: pycodestyle corrected

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3 
-
+#!/usr/bin/env python3
 
 # Define variables
 number = __import__('random').randint(-10000, 10000)
 if number < 0:
-	digit = number % -10
+    digit = number % -10
 else:
     digit = number % 10
 message = (f"Last digit of {number} is {digit}")
@@ -12,8 +11,8 @@ message = (f"Last digit of {number} is {digit}")
 
 # Output info
 if (digit > 5):
-      print(f"{message} and is greater than 5")
+    print(f"{message} and is greater than 5")
 elif (number == 0):
-      print(f"{message} and is 0 ")
+    print(f"{message} and is 0 ")
 else:
-      print(f"{message} and is less than 6 and not 0")
+    print(f"{message} and is less than 6 and not 0")
