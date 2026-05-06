29b4cf77e7fc0310d8ea877e11711543f009cb8c
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-06 17:58:48 +0200

    Fix: change the variable inside the condition line 15 for handle situation when digit is 0

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -12,7 +12,7 @@ message = (f"Last digit of {number} is {digit}")
 # Output info
 if (digit > 5):
     print(f"{message} and is greater than 5")
-elif (number == 0):
+elif (digit == 0):
     print(f"{message} and is 0 ")
 else:
     print(f"{message} and is less than 6 and not 0")
