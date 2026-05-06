4020a638fe971cd31ef3c1db8973f165905a6332
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-06 10:35:40 +0200

    Fix: Delete unused variable

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 # Define variables
-index = 0
 
 # Output info
 for index in range(97, 123):
     if index != 101 and index != 113:
         print(chr(index), end="")
+    if index >= 123:
+        print(" ")
