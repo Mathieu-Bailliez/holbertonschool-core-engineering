4e715f9e6874e5dae31bd59cdc4b834afab71b5c
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-06 23:47:11 +0200

    Return True is c is a lowercase letter , false otherwise

---
+++ python_fundamentals/functions_modules/islower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def islower(c):
+    """
+    Returns True if c is a lowercase letter , False otherwise.
+    """
+    ascii_value = ord(c)
+    
+    if ascii_value >= 97 and ascii_value <= 122:
+        return True
+    
+    return False
