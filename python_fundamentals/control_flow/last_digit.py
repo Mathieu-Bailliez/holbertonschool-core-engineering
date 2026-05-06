806a2f9c1bfeeced23e6c256d877cccece4224cd
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-06 17:50:04 +0200

    Fix: add condition on variables declaration and handle the negatif modulo

---
+++ python_fundamentals/control_flow/last_digit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3 
+
+
+# Define variables
+number = __import__('random').randint(-10000, 10000)
+if number < 0:
+	digit = number % -10
+else:
+    digit = number % 10
+message = (f"Last digit of {number} is {digit}")
+
+
+# Output info
+if (digit > 5):
+      print(f"{message} and is greater than 5")
+elif (number == 0):
+      print(f"{message} and is 0 ")
+else:
+      print(f"{message} and is less than 6 and not 0")
