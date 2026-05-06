78622dbbbc4cf7ffa8c77f5dd8e11aa087edf5fd
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-06 18:33:03 +0200

    Feat: delete the 2nd print and handle differently the newline for respect holberton rules

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 # Define variables
+alphabet = ""
 
 # Output info
 for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("{}".format(chr(i)), end="")
-print()
+    if chr(i) != 'q' and chr(i) != 'e':
+        alphabet += chr(i)
+print("{}".format(alphabet), end="\n")
