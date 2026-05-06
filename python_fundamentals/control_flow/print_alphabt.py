61b03a2bb78e6c3b046adb7c0b46c18f29e36a62
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-06 18:09:49 +0200

    Feat: Add THAT FINAL (newline) juts by printing nothing after the loop

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -3,8 +3,7 @@
 # Define variables
 
 # Output info
-for index in range(97, 123):
-    if index != 101 and index != 113:
-        print(chr(index), end="")
-    if index >= 122:
-        print("")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
+print()
