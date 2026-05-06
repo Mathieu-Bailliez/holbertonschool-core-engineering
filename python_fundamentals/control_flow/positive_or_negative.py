aec0a7289c1931acbaa98ff31f65432119ec173c
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-06 08:45:28 +0200

    rename: variable

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -2,14 +2,14 @@
 
 # Define variables
 number = __import__('random').randint(-10, 10)
-message_negatif = "is negative"
-message_positive = "is positive"
-message_zero = "is zero"
+negatif = "is negative"
+positive = "is positive"
+zero = "is zero"
 
 # Output info
 if (number > 0):
-    print(f"{number} {message_positive}")
+    print(f"{number} {positive}")
 elif (number == 0):
-    print(f"{number} {message_zero}")
+    print(f"{number} {zero}")
 else:
-    print(f"{number} {message_negatif}")
+    print(f"{number} {negatif}")
