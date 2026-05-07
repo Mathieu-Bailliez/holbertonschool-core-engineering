e5cc08aed35e19cb710e6bb4b170b295a6e5be02
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-07 21:39:01 +0200

    Fix: respect pycodestyle and holberton rules

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,18 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 def uppercase(str):
     for c in str:
-        ascii_value = ord(c)
-
-        if 97 <= ascii_value <= 122:
-            print(chr(ascii_value - 32), end="")
+        if 97 <= ord(c) <= 122:
+            print(chr(ord(c) - 32), end="")
         else:
             print(c, end="")
-
     print()
-
-if __name__ == "__main__":
-    uppercase("Holberton")
-    uppercase("hello world")
-    uppercase("Python123")
-    
