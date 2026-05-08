1d1bd1f19fa9cc4c2293da29ec9f31a5535abe2a
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-08 14:18:50 +0200

    Feat: more readables file

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
 # Define variables
+
 number = __import__('random').randint(-10, 10)
 negatif = "is negative"
 positive = "is positive"
 zero = "is zero"
 
 # Output info
+
 if (number > 0):
+
     print(f"{number} {positive}")
+
 elif (number == 0):
+
     print(f"{number} {zero}")
+
 else:
+    
     print(f"{number} {negatif}")
