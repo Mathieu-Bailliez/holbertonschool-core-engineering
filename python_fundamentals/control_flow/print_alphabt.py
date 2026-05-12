3f946a03a6879e22bd205e116f89064ec26039b7
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-12 10:58:35 +0200

    Fix: Print alphabet with one print() with the \n and no space between letters

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python3
 
-# Define variables
-alphabet = ""
 
-# Output info
-for i in range(97, 123):
-    if chr(i) != 'q' and chr(i) != 'e':
-        alphabet += chr(i)
-print("{}".format(alphabet), end="\n")
+"""Print the lowercase alphabet except for q and e.
+
+Output must be continuous (no spaces, no new lines
+except at end).
+
+You can only use print once in your code """
+
+
+"""Expected pattern:
+
+abcdfghijklmnoprstuvwxyz
+"""
+
+"""Repo:
+
+    GitHub repository: holbertonschool-core-engineering
+    Directory: python_fundamentals/control_flow
+    File: print_alphabt.py
+"""
+
+"""1) How to print alphabet"""
+
+alphabet = ("abcdefghijklmnopqrstuvwxyz\n")
+
+"""2) How to add a new line at end"""
+
+count = 0
+
+for count in alphabet:
+    if count != 'e' and count != 'q':
+        print(count, end="")
+
+
