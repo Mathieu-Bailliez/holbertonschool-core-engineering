f4a4bcd776f7e3d82f5af0ed974b99a842dec0a1
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-15 16:03:31 +0200

    Print up to x element from a list

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -5,13 +5,20 @@ Prints up to x elements from my_list on one line.
 Returns the actual number of elements printed.
 """
 
+
 def safe_print_list(my_list=[], x=0):
 
+    count = 0
 
-    try:
-        print(x)
-    except:
-        print("An errors occured")
-    finally:
-        print("Variables x is not Defined")
-    return(x)
+    while count < x:
+        try:
+            print(my_list[count], end="")
+
+            count += 1
+
+        except IndexError:
+            break
+
+    print()
+
+    return count
