dd2657b6b59174472009cb57b208404ea38316af
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-28 16:21:13 +0200

    Add: Docstrings

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-
+"""Module write_file"""
 
 def write_file(filename="", text=""):
-
+    """Write an UTF-8 file and returns the number of characters written"""
     with open(filename, 'w', encoding="utf-8") as f:
-
         number = f.write(text)
         return number
