89e43fc1087f19dd37008ef57a9feecde415790a
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-28 16:25:29 +0200

    Add: Docstrings

--- python_advanced/file_handling/append_write.py
+++ python_advanced/file_handling/append_write.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-
+"""Module append_file"""
 
 def append_write(filename="", text=""):
+    """appends a string at the end of a text file (UTF-8)\
+    and returns the number of characters added"""
 
     with open(filename, "a", encoding="utf-8") as f:
-
         nb_character = f.write(text)
-
         return nb_character
