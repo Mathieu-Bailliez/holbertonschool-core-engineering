517d6b611e25a707664cb901172767ba344f3288
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-28 16:16:38 +0200

    Add: The docstring of the file and delete the variable stockage

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
+"""Module read_file"""
 
-# a function that reads a text file
-# UTF 8 and print it to STDOUT
-# USE THE WITH STATEMENT
-# NO NEED TO MANAGE FILE PERMISSION
-# OR FILE DOEST EXIT EXCEPTIONS
-# 0 NOT ALLOWED TO USE IMPORT ANY MODULE
 
 def read_file(filename=""):
-
+    """Read a UTF-8 file and print its content to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
-        content = f.read()
-    print(content, end="")
-
-
+        print(f.read(), end="")
