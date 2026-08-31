24e06ff2142ec6b4d17b28d9c8a25d2dc28ca31c
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-08-31 22:45:56 +0200

    Fix: delete the variable accumulation for respect the constraint

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -4,36 +4,10 @@
 
 # 2. Alphabet Game (Lowercase)
 
-# Etape 1. On crée une variable vide pour stocker les lettres
-result = ""
-
-# Etape 2. On boucle jusqu'à 123 pour inclure le 'z' (122)
-for alphabet in range(97, 123):
-    if alphabet != 101 and alphabet != 113:
-        # Etape 3. On ajoute la lettre à notre variable
-        result = result + chr(alphabet)
-
-# Etape 4. On affiche tout d'un coup (le saut de ligne automatique est préservé !)
-print(result)
-
-
-# Ce qui ma bloquer
-
-"""
-J'ai tenter de créer une variable qui contient abcd...z et ca marche mais la boucle for n'est pas utiliser
-dans ce cas
-
-le dernier saut de ligne était ce qui ma bloquer le plus longtemps en un SEUL print sinon il fallait systématiquement
-mettre un deuxiéme print() vide aprés la boucle
-"""
-
-
-# Ce que j'ai appris :
-
-"""
-J'ai appris a utiliser une variable pour stocker l'accumulation et le résultat de
-la boucle
-ce qui permet ensuite avec un seul print d'avoir le comportement normal de saut de ligne
-avec le end="" tout en ayant les caractére coller entre eux
-Tout ca en UN seul Print
-"""
+# Step 1. We loop up to 123 to include 'z' (122)
+for letter in range(97, 123):
+    # Step 2. We only print if letter is not e or q
+    if letter != 101 and letter != 113:
+        # Step 3. We control the output using the end parameter
+        # and format with :c
+        print(f"{letter:c}", end="\n" if letter == 122 else "")
