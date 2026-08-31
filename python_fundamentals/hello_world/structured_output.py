f64cef5daa2152f3fb95d9ed59b992df4e9fa5f9
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-08-31 13:30:16 +0200

    Add: print special Ouput using formating interpolation

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module: Create executable file thats prints : langage , version , pi , and bool for my first approach with python"""
+
+# 1. Deterministic Script Output
+
+langage = "Python"
+version = 3
+pi = 3.14159265359
+my_bool = version > 2
+
+print(f"Language: {langage}")
+print(f"Version: {version}")
+print(f"Pi approx: {pi:.2f}")
+print(f"Computation valid: {my_bool}")
