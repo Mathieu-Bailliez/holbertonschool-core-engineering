2d87c99ae9c21b7236c9616cfdb80fb68e4c91bb
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-08-31 20:08:06 +0200

    Fix: the line 6 with the import module , vscode transforme single quote ,into double quote

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-number = __import__("random").randint(-10, 10)
+number = __import__('random').randint(-10, 10)
 positive = "is positive"
 zero = "is zero"
 negative = "is negative"
@@ -11,3 +11,15 @@ elif number == 0:
     print(f"{number} {zero}")
 else:
     print(f"{number} {negative}")
+
+# What blocked me
+"""
+VS Code automatically changed the single quotes `' '` to double quotes `" "`,
+which caused a mismatch with the exact line required by the project.
+"""
+
+# What I understood
+"""
+Be careful with the software you use to avoid copy-paste issues when
+transferring code from the instructions to your editor.
+"""
