15ad9dbb8c1a6d0cec855732b9fafe2340945729
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-08-31 19:57:03 +0200

    Fix: Clear the doc and fix positive

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
-"""Module: Generate a ramdom number and print if the result  is zero, positive
-or négative"""
 
-# Define variables
 number = __import__("random").randint(-10, 10)
-
 positive = "is positive"
 zero = "is zero"
 negative = "is negative"
 
-# Output:
 if number > 0:
     print(f"{number} {positive}")
 elif number == 0:
