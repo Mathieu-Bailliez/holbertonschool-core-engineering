6c6d1f3241ee7b3ded0dc340f7bbc59ae6c5ed3d
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-08-31 22:57:01 +0200

    Fix: delete the newline at the end of the file for strout lenght == 24 insteaf of 25 with the newline

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -5,9 +5,9 @@
 # 2. Alphabet Game (Lowercase)
 
 # Step 1. We loop up to 123 to include 'z' (122)
-for leter in range(97, 123):
-    # Step 2. We only print if leter is not e or q
-    if leter != 101 and leter != 113:
+for letter in range(97, 123):
+    # Step 2. We only print if letter is not e or q
+    if letter != 101 and letter != 113:
         # Step 3. We control the output using the end parameter
         # and format with :c
-        print("{:c}".format(leter), end="\n" if leter == 122 else "")
+        print("{:c}".format(letter), end="")
