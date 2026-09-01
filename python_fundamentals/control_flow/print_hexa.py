64b6c0e26b2ee903fe62718bb15ff9d889d4f02e
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-01 10:53:18 +0200

    Fix: use the .format required instead of f-string

--- python_fundamentals/control_flow/print_hexa.py
+++ python_fundamentals/control_flow/print_hexa.py
@@ -2,4 +2,7 @@
 """Module : """
 
 for number in range(99):
-    print(f"{number:d} = {hex(number)}")
+    # print(f"{number} = 0x{number:x}") # f-string version
+    # print("{:d} = 0x{:x}".format(number, number)) # Appeler deux fois la variable
+    print("{0} = 0x{0:x}".format(number)) # Utiliser l'index 0 deux fois
+
