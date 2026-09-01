5427469b4554ba910467bc11c2fbfc30221f97ab
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-01 18:41:50 +0200

    Fix: Decompose the syntax into multiple line for holberton constraint

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -4,4 +4,7 @@
 for digit1 in range(10):
     for digit2 in range(10):
         if digit1 < digit2:
-            print(f"{digit1}{digit2}", end="\n" if digit1 == 8 else ", ")
+            if digit1 == 8:
+                print("{}{}".format(digit1, digit2), end="\n")
+            else:
+                print("{}{}".format(digit1, digit2), end=", ")
