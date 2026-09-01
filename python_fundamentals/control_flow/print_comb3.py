d7d31f10ec008d7bd7f7b84852ff9459e0ba5048
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-01 18:36:09 +0200

    Fix: pycodestyle line was too long , i add \ at the end

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -4,4 +4,5 @@
 for digit1 in range(10):
     for digit2 in range(10):
         if digit1 < digit2:
-            print("{}{}".format(digit1, digit2), end="\n" if digit1 == 8 else ", ")
+            print("{}{}".format(digit1, digit2), \
+                  end="\n" if digit1 == 8 else ", ")
