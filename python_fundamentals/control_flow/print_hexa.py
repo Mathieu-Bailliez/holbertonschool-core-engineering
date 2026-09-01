91dcef5f866397debe1d5274381568b2d563b4e5
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-01 11:01:16 +0200

    Fix: delete commentary print

--- python_fundamentals/control_flow/print_hexa.py
+++ python_fundamentals/control_flow/print_hexa.py
@@ -2,7 +2,5 @@
 """Module : """
 
 for number in range(99):
-    # print(f"{number} = 0x{number:x}") # f-string version
-    # print("{:d} = 0x{:x}".format(number, number)) # Appeler deux fois la variable
     print("{0} = 0x{0:x}".format(number)) # Utiliser l'index 0 deux fois
 
