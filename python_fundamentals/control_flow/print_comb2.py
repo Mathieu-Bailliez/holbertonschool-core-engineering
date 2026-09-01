d794440f52325615ab1e04574d15e10657660577
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-01 13:57:32 +0200

    Add: the new line after the last number

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -2,4 +2,4 @@
 """Module : """
 
 for number in range(100):
-    print("{0:02}".format(number), end=", " if number < 99 else "")
+    print("{0:02}".format(number), end=", " if number < 99 else "\n")
