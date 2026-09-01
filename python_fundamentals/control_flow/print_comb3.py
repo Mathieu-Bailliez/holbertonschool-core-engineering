4e9efbf87338b13281dd5b52fdbc1d07549579bc
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-01 18:38:17 +0200

    Fix: change .format into f-string for reduce line lenght

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -4,5 +4,4 @@
 for digit1 in range(10):
     for digit2 in range(10):
         if digit1 < digit2:
-            print("{}{}".format(digit1, digit2), \
-                  end="\n" if digit1 == 8 else ", ")
+            print(f"{digit1}{digit2}", end="\n" if digit1 == 8 else ", ")
