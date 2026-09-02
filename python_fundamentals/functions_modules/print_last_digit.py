6d6100bfa1829443fc7c06076155a9cb60398b74
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-02 13:59:10 +0200

    Add: elif case for number == 0

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -6,8 +6,10 @@ def print_last_digit(number):
 
     if number < 0:
         digit = number % -10
+    elif number == 0:
+        digit = 0
     else:
         digit = number % 10
 
-    print(abs(digit))
-    return abs(digit)
+    print(digit)
+    return(digit)
