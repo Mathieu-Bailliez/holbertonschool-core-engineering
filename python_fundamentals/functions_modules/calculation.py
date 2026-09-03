f2406ba220f1cbf75148c28efcd1095735afeec5
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-03 14:04:21 +0200

    Fix: add the missing import guard

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -4,18 +4,19 @@
 
 from calculator_1 import add, div, mul, sub
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-sum = add(a, b)
-difference = sub(a, b)
-product = mul(a, b)
-quotient = div(a, b)
+    sum = add(a, b)
+    difference = sub(a, b)
+    product = mul(a, b)
+    quotient = div(a, b)
 
-print("{} + {} = {}".format(a, b, sum))
+    print("{} + {} = {}".format(a, b, sum))
 
-print("{} - {} = {}".format(a, b, difference))
+    print("{} - {} = {}".format(a, b, difference))
 
-print("{} * {} = {}".format(a, b, product))
+    print("{} * {} = {}".format(a, b, product))
 
-print("{} / {} = {}".format(a, b, quotient))
+    print("{} / {} = {}".format(a, b, quotient))
