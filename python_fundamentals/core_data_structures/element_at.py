3982ceb68cb1ae95f515e164d4a137e5efae9b85
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-04 10:18:03 +0200

    Fix: delete useless commentary

--- python_fundamentals/core_data_structures/element_at.py
+++ python_fundamentals/core_data_structures/element_at.py
@@ -4,8 +4,6 @@
 
 def element_at(my_list, idx):
 
-    # recherche moi index 3 dans la list
-    # determiner la longueur de la list
     if idx < 0 or idx >= len(my_list):
         return None
     else:
