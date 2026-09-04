0f1eb7583cf2b51f92c1dbf804436925d350d49e
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-04 10:12:55 +0200

    Fix: replace operand >= in the condition

--- python_fundamentals/core_data_structures/element_at.py
+++ python_fundamentals/core_data_structures/element_at.py
@@ -6,8 +6,8 @@ def element_at(my_list, idx):
 
     # recherche moi index 3 dans la list
     # determiner la longueur de la list
-
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
         return my_list[idx]
+
