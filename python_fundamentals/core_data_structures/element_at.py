ee58b34724d98533b37a46be66ee24fd2fc77bee
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-04 10:00:23 +0200

    Add: return element at a given index

---
+++ python_fundamentals/core_data_structures/element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module: """
+
+
+def element_at(my_list, idx):
+
+    # recherche moi index 3 dans la list
+    # determiner la longueur de la list
+
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        return my_list[idx]
