7f49eb6f5c3233ab01ba413844f2afc5ec7cf71f
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-04 13:06:09 +0200

    Add: print all element in the matrix

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module: """
+
+
+def print_matrix_integer(matrix=[[]]):
+
+    row1 = matrix[0]
+    row2 = matrix[1]
+    row3 = matrix[2]
+
+    print(*row1)
+    print(*row2)
+    print(*row3)
