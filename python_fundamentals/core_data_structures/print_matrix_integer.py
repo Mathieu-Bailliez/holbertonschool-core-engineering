4960e92fa9a70011f7992e33a40e98388b905a92
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-04 13:08:01 +0200

    Fix: use .format pattern

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -8,6 +8,6 @@ def print_matrix_integer(matrix=[[]]):
     row2 = matrix[1]
     row3 = matrix[2]
 
-    print(*row1)
-    print(*row2)
-    print(*row3)
+    print("{}".format(*row1))
+    print("{}".format(*row2))
+    print("{}".format(*row3))
