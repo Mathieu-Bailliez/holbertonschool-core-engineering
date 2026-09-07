52c09341e1d15a4154734ff4bb0e5083f82030c8
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-07 20:21:54 +0200

    Add: raise a NameError with a message

---
+++ python_fundamentals/exception_handling/raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module: function that intentionally raised a TypeError"""
+
+
+def raise_exception_msg(message=""):
+    """Raise a TypeError"""
+    raise NameError(message)
