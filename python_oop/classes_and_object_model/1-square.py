8dc963a44f073586f11764dae99a3be14b5c33c0
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-08 14:33:59 +0200

    Add: private instance attribute to the same class Square and docstring for the def init method

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module: Defines a Square class."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square instance with a private instance
+        attributes"""
+        self.__size = size
