0f92bec8db47a78c0d8487aa36ba9a6dfdb3a8a0
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-08 17:33:54 +0200

    Initialize Square with default size and validate size

---
+++ python_oop/classes_and_object_model/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module: Defines a Square class."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square instance with a private instance
+        attributes"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
