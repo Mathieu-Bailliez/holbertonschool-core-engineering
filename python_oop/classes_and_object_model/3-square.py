57e98198156d0a13d2dda27624ebe77edc12f6a2
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-08 17:45:07 +0200

    Add: method area for calculate the area of the Square depending on the state of the self.__size given argument

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module: Defines a Square class."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square instance with a private instance
+        attributes"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
