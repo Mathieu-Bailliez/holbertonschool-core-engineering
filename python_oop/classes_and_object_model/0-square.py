df9f52773965d2d34920572cd75f700c8a6d5da2
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-08 12:07:11 +0200

    Directory for python oop and first task , Create my first class Square

---
+++ python_oop/classes_and_object_model/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Module: Create a Class Square using special methode"""
+
+
+class Square:
+    def __init__
---
+++ python_oop/classes_and_object_model/README.md
@@ -0,0 +1,3 @@
+# [classes_and_object_model](./)
+
+## [0-square.py](./0-square.py) - Create my first class (empty)
