3d11afd3dbe46ec36b372a29b4e6dedb29b1d640
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-09 21:24:05 +0200

    feat: add BaseGeometry base class

---
+++ python_oop/inheritance/base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Define the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Represent the base geometry class for all shapes."""
+
+    def area(self):
+        """Calculate and return the area of the geometry."""
+
+        raise Exception("area() is not implemented")
