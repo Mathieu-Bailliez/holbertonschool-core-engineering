d5155bfe7a7d42a4aeb41d52a17ef2892142d043
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-10 15:14:17 +0200

    Add Square with its own string representation

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Create a Square class based on Rectangle class itself based
+on BaseGeometry"""
+
+
+Rectangle = __import__('2-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square defined by a private size."""
+
+    def __init__(self, size):
+        """Initialize a Square with a validated size."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return the string representation of the square."""
+        return f"[Square] {self.__size}/{self.__size}"
