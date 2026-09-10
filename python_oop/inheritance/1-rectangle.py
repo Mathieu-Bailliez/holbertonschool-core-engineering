602b246969e1d9a4fc31c4d4af945b6bce5c6ac7
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-10 14:13:06 +0200

    Fix Rectangle to inherit from imported BaseGeometry

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,30 +1,17 @@
 #!/usr/bin/env python3
-"""Define the BaseGeometry class."""
+"""Module that defines a Rectangle class based on BaseGeometry class."""
 
 
-class BaseGeometry:
-    """Represent the base geometry class for all shapes."""
-
-    def area(self):
-        """Calculate and return the area of the geometry."""
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate that value is a positive integer."""
-
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Set a width and height validated with the integer_validator() method
-    inherited from the parent class BaseGeometry"""
+    """Rectangle defined by a private width and height."""
 
     def __init__(self, width, height):
-        self.integer_validator(width, width)
-        self.integer_validator(height, height)
+        """Initialize a Rectangle with validated width and height."""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
