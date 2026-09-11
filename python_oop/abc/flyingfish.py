5e0096382b1af28ae0a15e7dbfc837598fb9ecc2
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-11 14:28:04 +0200

    Add FlyingFish example demonstrating multiple inheritance

---
+++ python_oop/abc/flyingfish.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Demonstrate multiple inheritance with a FlyingFish class."""
+
+
+class Fish:
+    "Represents a fish"
+
+    def swin(self):
+        """Print how the fish moves."""
+
+        print("The fish is swimming")
+
+    def habitat(self):
+        """Print where the fish lives."""
+
+        print("The fish lives in water")
+
+
+class Bird:
+    "Represents a bird"
+
+    def fly(self):
+        """Print how the bird moves."""
+
+        print("The bird is flying")
+
+    def habitat(self):
+        """Print where the bird lives."""
+
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """Represent a flying fish, inheriting from both Fish and Bird."""
+
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swin(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
+
+
+ff = FlyingFish()
+
+ff.fly()
+ff.swin()
+ff.habitat()
+print(type(ff).mro())
