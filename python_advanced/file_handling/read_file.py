147632b15586ef6d86d72cdf6b35f2f5df004281
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-09-21 22:15:16 +0200

    Fix: pycodestyle and docstrings

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
+"""Provide a function that prints the contents of a UTF-8 text file."""
+
 
 def read_file(filename=""):
+    """Read a UTF-8 text file and print its contents to stdout."""
     with open(filename, mode="r", encoding="utf-8") as f:
         content = f.read()
         print(content, end="")
-
-
-if __name__ == "__main__":
-    read_file("my_file_0.txt")
